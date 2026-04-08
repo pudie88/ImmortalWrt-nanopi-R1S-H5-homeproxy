fda888c809bbb26ee6fc03e5ce7677598fc44042
pudie88 <58283163+pudie88@users.noreply.github.com>
2026-04-08 14:19:10 +0800

    Create diy-part2.sh

---
+++ SCRIPTS/R1S-H5/diy-part2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+
+# 修改默认 LAN IP
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
